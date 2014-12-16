83afc59a50d67cc5420147792653d5510a606de8
BenFradet <benjamin.fradet@gmail.com>
2014-12-16 14:32:28 +0100

    modified dataset

--- TP3/dataProcessing.R
+++ TP3/dataProcessing.R
@@ -38,11 +38,17 @@ noRainSubset$weather <- ifelse(noRainSubset$cloudLevel >= 6, 'Cloudy', 'Sunny')
 data$weather[data$weather == 'Rain' & data$precipitation == 0] <-
     noRainSubset$weather
 data$weather <- factor(data$weather)
+normalPressure <- 1013.25
+data$state <- ifelse(data$pressure <= normalPressure, 1, 2)
+data$state <- factor(data$state)
 # 1 = Cloudy
 # 2 = Rain
 # 3 = Snow
 # 4 = Sunny
 data$weather2 <- unclass(data$weather)
+# 1 = Low pressure
+# 2 = High pressure
+data$state2 <- unclass(data$state)
 data$dayOfYear <- c(1:365)
 
 write.csv(data, file = 'processedDataset.csv', quote = F, row.names = F)
